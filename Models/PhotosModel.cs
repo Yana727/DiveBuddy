3ec51d91b2e7affffe24350f680b6cd929793bf1
Yana Wilkins <yav9774@gmail.com>
2017-10-02 16:13:58 -0400

    added model for photos

--- Models/PhotosModel.cs
+++ Models/PhotosModel.cs
@@ -1,7 +1,24 @@
+using System;
+using DiveBuddy.Models;
+
 namespace DiveBuddy 
 { 
     public class PhotosModel 
     {
-      public url PicUrl { get; set; }
+      public Guid Id { get; set; }
+      public string Name { get; set; }
+      public byte[] Data { get; set; }
+      public int Width { get; set; }
+      public int Height { get; set; }
+      public string ContentType { get; set; }
+      
+
+       //FK : User
+        public string ApplicationUserID { get; set; }
+        public virtual ApplicationUser ApplicationUser {get; set;}
+
+        //FK : Buisness
+        public int BuisnessId { get; set; }
+        public BuisnessModel Buisness { get; set; }
     }
 }
