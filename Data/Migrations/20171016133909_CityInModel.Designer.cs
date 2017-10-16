5a1e900888ccc39356b6ae5855943d8d58590c35
Yana Wilkins <yav9774@gmail.com>
2017-10-16 09:45:24 -0400

    Added City to Buisness Views

--- Data/Migrations/20171016133909_CityInModel.Designer.cs
+++ Data/Migrations/20171016133909_CityInModel.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace DiveBuddy.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171016133909_CityInModel")]
+    partial class CityInModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20171016133909_CityInModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DiveBuddy.Data.Migrations
+{
+    public partial class CityInModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "BuisnessModel",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "BuisnessModel");
+        }
+    }
+}
